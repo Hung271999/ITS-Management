9e6849ce277d1f9d062063a8b0186f6cf60e8449
Hung <Hung71999@gmail.com>
2024-07-10 14:10:10 +0700

    chore: format code

--- src/main/java/com/sharp/vn/its/management/service/UserManagementService.java
+++ src/main/java/com/sharp/vn/its/management/service/UserManagementService.java
@@ -112,7 +112,8 @@ public class UserManagementService extends BaseService {
         final RoleEntity role = roleRepository.findByRoleName(itsRole)
                 .orElseThrow(() -> {
                     log.error("Can't find role with name: {}", itsRole.name());
-                    return new ObjectNotFoundException(MessageCode.ERROR_USER_CANNOT_FIND_ROLE_WITH_NAME);
+                    return new ObjectNotFoundException(
+                            MessageCode.ERROR_USER_CANNOT_FIND_ROLE_WITH_NAME);
                 });
         UserRoleEntity userRole = new UserRoleEntity(user, role);
         user.getRoles().add(userRole);
@@ -132,15 +133,13 @@ public class UserManagementService extends BaseService {
             log.error("User ID is null or empty");
             throw new DataValidationException(MessageCode.ERROR_USER_ID_EMPTY_OR_NULL);
         }
-        try {
-            Boolean hasTasks = taskRepository.existsByPersonInChargeId(id);
-            if (hasTasks) {
-                log.error("User with id {} can not delete because task associate", id);
-                throw new DataValidationException(MessageCode.ERROR_USER_WITH_FOREIGN_KEY_TO_TASK);
-            }
-            userRepository.deleteById(id);
-            log.info("User with id {} deleted successfully.", id);
-        } catch (DataIntegrityViolationException e) {}
+        Boolean hasTasks = taskRepository.existsByPersonInChargeId(id);
+        if (hasTasks) {
+            log.error("User with id {} can not delete because task associate", id);
+            throw new DataValidationException(MessageCode.ERROR_USER_WITH_FOREIGN_KEY_TO_TASK);
+        }
+        userRepository.deleteById(id);
+        log.info("User with id {} deleted successfully.", id);
     }
 
 
@@ -171,7 +170,7 @@ public class UserManagementService extends BaseService {
         }
         log.info("Fetching user detail for id: {}", id);
         final UserDTO userDTO = new UserDTO(userRepository.findById(id).orElseThrow(() -> {
-            log.error("User not found with id: {}",id);
+            log.error("User not found with id: {}", id);
             return new ObjectNotFoundException(MessageCode.ERROR_USER_ID_NOT_FOUND);
         }));
         log.info("User detail fetched successfully for id: {}", id);
@@ -192,7 +191,8 @@ public class UserManagementService extends BaseService {
         buildSortCondition(sort);
 
         Specification<UserEntity> specification = buildFilterCondition(searchParam);
-        Page<UserEntity> pageable = userRepository.findAll(specification, request.getFilter().getPageable());
+        Page<UserEntity> pageable =
+                userRepository.findAll(specification, request.getFilter().getPageable());
         log.info("All users fetched successfully.");
         return pageable.map(UserDTO::new);
     }
@@ -222,7 +222,7 @@ public class UserManagementService extends BaseService {
                     return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
                 };
     }
-    ;
+    
     private void buildSortCondition(Map<String, SortCriteria> sort) {
         if (sort == null || sort.isEmpty()) {
             sort.put("id", new SortCriteria("id", SortType.DESC.getText()));
