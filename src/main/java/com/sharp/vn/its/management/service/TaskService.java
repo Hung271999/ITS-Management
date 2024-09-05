a707b6793cc5d47b63948c0eda06289058b2e00a
hatornado <hahrs22@gmail.com>
2024-09-05 11:10:38 +0700

    add: add comment

--- src/main/java/com/sharp/vn/its/management/service/TaskService.java
+++ src/main/java/com/sharp/vn/its/management/service/TaskService.java
@@ -627,6 +627,12 @@ public class TaskService extends BaseService {
         return new TaskDataDTO(taskSummaryDTO,taskDataItems);
     }
 
+    /**
+     * Find effort of system by week task data dto.
+     *
+     * @param filter the filter
+     * @return the task data dto
+     */
     public TaskDataDTO findEffortOfSystemByWeek(TaskFilter filter) {
         List<TaskData> data = taskRepository.findTotalEffortSystemByWeek(filter.getSystemIds(),filter.getYears(),filter.getWeeks() );
         Map<Integer, List<TaskData>> mapGroupByWeek = data.stream().collect(Collectors.groupingBy(TaskData::getWeek, TreeMap::new, Collectors.toList()));
